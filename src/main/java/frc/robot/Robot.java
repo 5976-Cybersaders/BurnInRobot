9d243c1ad271b89b35b98df709c077d4251840c7
GoldenEXPn <zty1378212047@gmail.com>
2020-01-18 13:03:46 -0600

    updated robot comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,10 +12,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
+ * The purpose of this robot is a general gearbox burnin robot that we can use from year to year.
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
